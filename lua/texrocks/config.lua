ac646d6bb697cc43f29ad195dba42585ec7a52e3
Wu, Zhenyu <wuzhenyu@ustc.edu>
2025-02-02 22:50:22 +0800

    :children_crossing: Pass require() to config

--- lua/texrocks/config.lua
+++ lua/texrocks/config.lua
@@ -1,9 +1,11 @@
 -- ~/.config/texmf/init.lua
 local home = os.getenv('HOME')
 local config = {
+    require = require,
     rocks_path = home .. '/.local/share/texmf',
     luarocks_config_path = home .. '/.luarocks/config-5.3.lua',
     luarocks_binary = 'luarocks'
 }
 loadfile(home .. '/.config/texmf/init.lua', 't', config)()
+config.require = nil
 return config
