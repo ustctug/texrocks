43b23f94ffa12e0d9d590dd1da597d89024834fa
Wu, Zhenyu <wuzhenyu@ustc.edu>
2025-02-02 21:54:46 +0800

    :bug: Fix loadfile()

--- lua/texrocks/config.lua
+++ lua/texrocks/config.lua
@@ -1,11 +1,9 @@
--- luacheck: ignore 111 113
----@diagnostic disable: undefined-global
 -- ~/.config/texmf/init.lua
 local home = os.getenv('HOME')
-loadfile(home .. '/.config/texmf/init.lua')()
-local c = config or {}
-config = nil
-c.rocks_path = c.rocks_path or home .. '/.local/share/texmf'
-c.luarocks_config_path = c.luarocks_config_path or home .. '/.luarocks/config-5.3.lua'
-c.luarocks_binary = c.luarocks_binary or 'luarocks'
-return c
+local config = {
+    rocks_path = home .. '/.local/share/texmf',
+    luarocks_config_path = home .. '/.luarocks/config-5.3.lua',
+    luarocks_binary = 'luarocks'
+}
+loadfile(home .. '/.config/texmf/init.lua', 't', config)()
+return config
