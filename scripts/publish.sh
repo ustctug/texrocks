bdfedba6678750e03b13a286117349bb41ca9479
Wu, Zhenyu <wuzhenyu@ustc.edu>
2025-08-01 17:37:08 +0800

    :hammer: Add scripts/publish.sh

---
+++ scripts/publish.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+cd "$(dirname "$(dirname "$(readlink -f "$0")")")"
+
+cd packages
+rm -f ./*/*.rock{,spec}
+for dir in ./*/; do
+  if [[ "$dir" = ./demo-*/ || "$dir" == ./initex/ ]]; then
+    continue
+  fi
+  cd "$dir"
+  lx generate-rockspec
+  luarocks upload --force ./*.rockspec
+  cd ..
+done
+rm -f ./*/*.rock{,spec}
