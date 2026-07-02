8ef083f7a095d7c2bb0bd15f882455dd65787c58
Wu, Zhenyu <wuzhenyu@ustc.edu>
2026-07-02 19:18:23 +0800

    :technologist: Add luahbtex to shell.nix

--- shell.nix
+++ shell.nix
@@ -2,6 +2,16 @@
   pkgs ? import <nixpkgs> { },
 }:
 
+let
+  nur =
+    import
+      (fetchTarball {
+        url = "https://github.com/nix-community/NUR/archive/2a187cd9c92887f2af5833696b510288844eb49b.tar.gz";
+      })
+      {
+        inherit pkgs;
+      };
+in
 with pkgs;
 mkShell {
   name = "texrocks";
@@ -9,6 +19,7 @@ mkShell {
     # how lx find lua
     pkg-config
     lux-cli
+    nur.repos.Freed-Wu.luahbtex
 
     rename
 
