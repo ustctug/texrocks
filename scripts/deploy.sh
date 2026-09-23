f94225fdbad2a2b70262d8282f63e8e5d7bc5008
Wu, Zhenyu <wuzhenyu@ustc.edu>
2026-09-24 02:36:12 +0800

    :construction_worker: Add deploy.yml

---
+++ scripts/deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -e
+WD="$(dirname "$(dirname "$(readlink -f "$0")")")"
+
+for i; do
+  luarocks install "$i"
+done
+luarocks list --porcelain |
+  sed 's/\(\S\+\).*/luarocks pack \1 \&\& luarocks download --rockspec \1/' |
+  sh
+
+# https://github.com/luarocks/luarocks/issues/1817
+for f in texrocks-*.rock texdef-*.rock; do
+  if [ -f "$f" ]; then
+    rename -f 's/linux-x86_64/all/' "$f"
+  fi
+done
+
+"$WD/scripts/upload.sh" ./*.rockspec
+luarocks-admin make-manifest .
+zip "manifest-$LUA_VERSION.zip" "manifest-$LUA_VERSION"
+"$WD/scripts/process-index.html.pl" index.html
+rm -f ./*.rockspec manifest{,-5.{1..5}}
